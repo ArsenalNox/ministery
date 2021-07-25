300a85d5a7217ae4660d2381acf714913070170a
ArsenalNox <nohtimore@gmail.com>
2021-07-25 23:59:38 +0500

    Много школ.

--- scripts/insertNewSchool/main.py
+++ scripts/insertNewSchool/main.py
@@ -1,4 +1,3 @@
-import glob
 import os
 import mysql.connector 
 
@@ -11,14 +10,45 @@ dbConn = mysql.connector.connect(
 
 cursor = dbConn.cursor()
 
-schoolDirectory = '50'
+schoolDirectory = '91'
 
 rootdir = "C:/xampp/htdocs/ministery/img/{}".format(schoolDirectory)
+categories_needed_count = 9
+current_categories = []
+categories = {
+	"1": "1",
+	"2": "2",
+	"3": "4",
+	"4": "3",
+	"5": "6",
+	"6": "5",
+	"7": "7",
+	"8": "8",
+	"9": "9"
+}
+
+iterator = 0
 
 for subdir, dirs, files in os.walk(rootdir):
 	print('iterating over file')
 	for file in files:
-		sql = "INSERT INTO `photos_edu`(`schid`, `category`, `path`) VALUES ({}, 1, 'img/{}/{}');".format(schoolDirectory, schoolDirectory, file)
+		iterator += 1
+
+		if '_' in file:
+			category = file.split('_')[0]
+		elif '-' in file:
+			category = file.split('-')[0]
+		
+		
+		if ( int(category) < 0) or ( int(category) > 9): 		
+			category = 1
+		if category not in current_categories:
+			current_categories.append(category)
+
+
+		sql = "INSERT INTO `photos_edu`(`schid`, `category`, `path`) VALUES ({}, {}, 'img/{}/{}');".format(schoolDirectory, categories[str(int(category))], schoolDirectory, file)
 		cursor.execute(sql)
 		print(sql)
+
+print("Photos added: {}\nCategories autodetected: {}".format(iterator, len(current_categories)))
 dbConn.commit()
