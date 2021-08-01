85a18bb8ece1777a147d09787bd17cab05e9fb25
ArsenalNox <nohtimore@gmail.com>
2021-08-01 17:02:52 +0500

    бд

--- min_db.sql
+++ min_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 29, 2021 at 09:12 PM
+-- Generation Time: Aug 01, 2021 at 01:59 PM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 7.3.26
 
@@ -5377,15 +5377,60 @@ INSERT INTO `photo_categories` (`id`, `name`) VALUES
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `uid` varchar(200) NOT NULL,
-  `pwd` varchar(200) NOT NULL
+  `pwd` varchar(200) NOT NULL,
+  `type` int(1) NOT NULL,
+  `region` int(11) DEFAULT NULL,
+  `token` varchar(200) DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `uid`, `pwd`) VALUES
-(1, 'admin', '$2y$10$zYjlG0Pecn2XQUOBDhjl6.G4RxEw8c5gnnRGK15gPQxDBvIoecoFO');
+INSERT INTO `users` (`id`, `uid`, `pwd`, `type`, `region`, `token`) VALUES
+(1, 'admin', '$2y$10$zYjlG0Pecn2XQUOBDhjl6.G4RxEw8c5gnnRGK15gPQxDBvIoecoFO', 1, NULL, '292b84fac37674d415857c013511aee4'),
+(86, 'M1-6150-MHJM', '$2y$10$z8.zohB52bZRmhu5oBXW9OFLN3ikhNeBKJBPqZCujbfHV6VJJcVg.', 0, 1, ''),
+(87, 'M2-8957-JMAZ', '$2y$10$Z75c9dxEWmuP90EYO.3pSuqhlJMdYc8B3IaGpXMpymtdgNZZjDuw.', 0, 2, ''),
+(88, 'M3-8603-CXBL', '$2y$10$e2FhrGO7FYowb/RmJ0zL4.AnK2zW6AbhsHJjd0ahNa8qNeoyuvgXe', 0, 3, ''),
+(89, 'M4-8241-SEZY', '$2y$10$4YluB.hZy9TcGHBQ1wn6Cu/DeXqS3L98k3qqCyJ4Y8s5m6FhC8ZLS', 0, 4, ''),
+(90, 'M5-9969-TZRB', '$2y$10$AjZZ/NILj8Th9K9WQ7x5W.MnXRLZ5Nnzjwhzrj09OuAnnuYiHUHV6', 0, 5, ''),
+(91, 'M6-5530-QHZZ', '$2y$10$HPkUF/2VHxT/KdPqvyVlvejNnkVD1JZv6OyoNIJglckz/0Gi6vq6m', 0, 6, ''),
+(92, 'M7-3491-XYMZ', '$2y$10$/yFl52anAtVlVXZfjracluxfjfaTPU0Ly/fIO3WFMd5CS8yVfeekG', 0, 7, ''),
+(93, 'M8-3380-JFGS', '$2y$10$p0dNd8C5Nm.MPXMMS4eaDeXYpTN/.ekPfbW1.8DcVZaifZpgTHX8a', 0, 8, ''),
+(94, 'M9-2593-YTSO', '$2y$10$11J7yDD6DBr8WmfkgBXYMujQnacLT52cwDo41q4q6/t5SbM0U1ESW', 0, 9, ''),
+(95, 'M10-5818-FNHG', '$2y$10$.oCC574udpxjFOYFu6wuEukJ/ywTpoTnR7sf7N2F202s//T5VU4OS', 0, 10, ''),
+(96, 'M11-4806-HFIP', '$2y$10$xzG9qqCGRmsnmnk4bC4QdeGVdC0Q/7YrVVX1l.wKMat0k7GGX2Tw6', 0, 11, ''),
+(97, 'M12-7259-FOOP', '$2y$10$TkOuIurH79docZ3juPqUfOxXOcAv7GaNK4xogEpvwLZwhuhQwhJLm', 0, 12, ''),
+(98, 'M13-6860-AGAA', '$2y$10$JQ.pwI6nSbTPzIcRPxktDOvCl4OXfbENFehaEEtH3whrMoUwjnMs6', 0, 13, ''),
+(99, 'M14-6478-JUHV', '$2y$10$w08TfcCgHYGB2uRfFWiqi.f7ozgq1gRa20UVcuOSIpXlYUBUteF1m', 0, 14, ''),
+(100, 'M15-3334-VUUS', '$2y$10$FTy505GUJO9oHgGSwY.4TeDNVuTykNoqAsong7NNS1bEy9apon8M6', 0, 15, ''),
+(101, 'M16-7707-UOEH', '$2y$10$kzYuodBkmj5DfNrxY/jLreO9RjvmIRh.HwWeb/OmPXMN5kGprjaP6', 0, 16, ''),
+(102, 'M17-7263-RBUA', '$2y$10$HjifxvGdN7Mg35DqmgCGoe7WDYJfnPZGMemkH0EJ8pDiLajdjZedu', 0, 17, ''),
+(103, 'M18-2784-AJUP', '$2y$10$kYlxxkcM3d57yQ/xGrXPc.hYHrjP77vCHNKZmInnlVPT2oe/PRvdu', 0, 18, ''),
+(104, 'M19-6034-YIBA', '$2y$10$mqcWdYxZjX8ajDz5JGAy5ekjlRjqqsoDlJ7Xm.UReI9pl2DRn1Kba', 0, 19, ''),
+(105, 'M20-4851-VMYB', '$2y$10$B2FwMiWa0peM2JU4tcVm5eN34PTlVnF6I2/SCkOnDXWsTnhYJH4FS', 0, 20, ''),
+(106, 'M21-7558-ZYBO', '$2y$10$aXixfCxg9m64x4ubSMH50umx5p9uZFkGm34YLNNmmx668lIMFiBxm', 0, 21, ''),
+(107, 'M22-3669-IBKP', '$2y$10$YUmRfIBjftDnNy0w2CDc2uVgpJcWgp/2nmPig69TJMhU1...Xh1Si', 0, 22, ''),
+(108, 'M23-6422-XWES', '$2y$10$Xloi/3YxFDayA5XvocaLhur1ihX.71TAIx3xn51yW0sCs29Z16SOe', 0, 23, ''),
+(109, 'M24-3905-AWWH', '$2y$10$5Uccd6Jl/BZJh2pZvTPxEuOeuJRSplMdFHV71op03IFh8eP2..uge', 0, 24, ''),
+(110, 'M25-9406-KHZJ', '$2y$10$aN5tUDLALyEjGfMwvvef8.gvtlF3CS6UKVbK6/Oqr4c6OhidgCHAe', 0, 25, ''),
+(111, 'M26-0733-VCST', '$2y$10$VRCUQnyXHSykMNj1q6Kk/eR2RfvXXPWQ.RVNLgBEjjvpaTRYlae2W', 0, 26, ''),
+(112, 'M27-2523-JXGT', '$2y$10$Jp9XNQdvvv0fUUGRWmoxpedgI4HGzQ6CvVX2E3nqQvo57qsEkb/Hi', 0, 27, ''),
+(113, 'M28-7878-DXGB', '$2y$10$T2GFdiM9gqb6uijjXqr2/.BO9mI1473YAheHNE7nnSHlP3HYV5Q9y', 0, 28, ''),
+(114, 'M29-9629-GWIF', '$2y$10$2fHo2VzjMOQ7w67x0hiEX.3Lr36qdsUJyRgOriyL.ZjHfgLpV3IWm', 0, 29, ''),
+(115, 'M30-4249-KJJG', '$2y$10$JblIUcdpO/Pnj75YNVnKF.1aZNLtgihwEJwAhehoZdHxD/NhhRj4O', 0, 30, ''),
+(116, 'M31-3234-YJAG', '$2y$10$sondBl8n.QCmOU2Hm3y8y.yBTTWKum6YHNWGDvduPQidHgVCAnJGq', 0, 31, ''),
+(117, 'M32-9527-OBXK', '$2y$10$aWpaAgXWSSX5yLouQbVpLubzIQRWziA7C/wcLuvcfhl6p8zYcbkyq', 0, 32, ''),
+(118, 'M33-2656-FFZQ', '$2y$10$39Gx0uYw5DNauLLrd7VPMew/wXjIflkZSL/mSrp7ceJPeuqv/ScBC', 0, 33, ''),
+(119, 'M34-8499-CCSP', '$2y$10$ulxohdPtD1KQ/nDod8K.0u9oBSSoeNvtmL7qQ4/CPgy4IwpQNVJpG', 0, 34, ''),
+(120, 'M35-6172-QHNK', '$2y$10$zlM5XVrMPqCnd9eZXLxxSORTTq062/UXKUirkR9PihYkAzNMBd2nS', 0, 35, '1f573dc12432fa318e722344e7f9cee8'),
+(121, 'M36-0186-OEUI', '$2y$10$3b8RWf7739SYkO9HKubQfeTt2H4Xg./Fh18IdNpnEJeb9GGhM2xVq', 0, 36, ''),
+(122, 'M37-8767-RLON', '$2y$10$HRdGEc.20HEngSiucYyhfOa9z8vgtAacpvackaYStTtQqC9d0Dlw2', 0, 37, ''),
+(123, 'M38-0815-QDTT', '$2y$10$OsSrB5o/QvXmWX1DA1bVDO6FxHzOHfomaNBHChwZhEy9QuvWurUfK', 0, 38, ''),
+(124, 'M39-6325-WNNL', '$2y$10$HeMcfIqozmcTU1pVNoggoeapyovr1Gtqi3pB3WAir3rIbHjar5aXy', 0, 39, ''),
+(125, 'M40-4480-YEBX', '$2y$10$Y7K251IwwxMWe3tAPqWyxOr3Tx/tJqQZ7i6aaaf74HSIORIwYyNRm', 0, 40, ''),
+(126, 'M41-9322-TXAK', '$2y$10$lXz2FeryAX5c4V/4mPPwmeMwqkNFtNwcctqbYZGc/XNMf3./A5lz6', 0, 41, ''),
+(127, 'M42-3057-GPJN', '$2y$10$6an/fft16E3jBZkx5rNl9.Nxne4s/C3xtkspX/r.1JRSlY5Th67O6', 0, 42, '');
 
 --
 -- Indexes for dumped tables
@@ -5428,7 +5473,8 @@ ALTER TABLE `photo_categories`
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `region` (`region`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -5462,7 +5508,7 @@ ALTER TABLE `photo_categories`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=128;
 
 --
 -- Constraints for dumped tables
@@ -5486,6 +5532,12 @@ ALTER TABLE `educational`
 ALTER TABLE `photos_edu`
   ADD CONSTRAINT `photos_edu_ibfk_1` FOREIGN KEY (`schid`) REFERENCES `educational` (`id`),
   ADD CONSTRAINT `photos_edu_ibfk_2` FOREIGN KEY (`category`) REFERENCES `photo_categories` (`id`);
+
+--
+-- Constraints for table `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`region`) REFERENCES `munipal` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
